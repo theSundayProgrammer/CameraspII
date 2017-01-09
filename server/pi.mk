91778a960d7c83b2174c55ae3122eca22ab86ab3
theSundayProgrammer <theSundayProgrammer@gmail.com>
2017-01-09 23:53:10 +0000

    Makefile changes to use boost

--- server/pi.mk
+++ server/pi.mk
@@ -1,6 +1,6 @@
 
 INCLUDES = -I ../include
-CXX_FLAGS = -std=c++14 -pthread -DASIO_STANDALONE -Wno-trigraphs
+CXX_FLAGS = -std=c++14 -pthread -DASIO_STANDALONE -Wtrigraphs
 BUILD_DIR=./build
 CXX=g++-6
 
@@ -15,7 +15,10 @@ objs = $(srcs:%.cpp=$(BUILD_DIR)/%.o)
 deps = $(srcs:.cpp=$(BUILD_DIR)/.d)
 
 camerasp: $(objs)
-	$(CXX)   -o $@ $^ -pthread -L/opt/vc/lib -L../lib -ljpeg -lvia-http -ljson -lmmal -lmmal_core -lmmal_util
+	$(CXX)   -o $@ $^ -pthread -L/opt/vc/lib \
+	-lboost_filesystem -lboost_system -ljpeg \
+	-L../lib -ljson \
+	-lmmal -lmmal_core -lmmal_util
 
 $(BUILD_DIR)/%.o: %.cpp
 	$(CXX) $(CXX_FLAGS) $(INCLUDES) $(OPTIONS) -MMD -MP -c $< -o $@
