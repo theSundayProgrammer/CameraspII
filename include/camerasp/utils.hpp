7c1088c630488f99e348c87772bb4e0a2abd6bec
theSundayProgrammer <theSundayProgrammer@gmail.com>
2017-02-09 15:15:56 +1100

    Added two functions to convert from string to enum
    RASPICAM_ enum conversion methods added but not yet used

--- include/camerasp/utils.hpp
+++ include/camerasp/utils.hpp
@@ -11,26 +11,25 @@
 #include <vector>
 #include <json/reader.h>
 #include <spdlog/spdlog.h>
+#include <camerasp/raspicamtypes.h>
 extern std::shared_ptr<spdlog::logger> console;
 #ifdef __GNUC__
 typedef int errno_t;
 errno_t fopen_s(FILE** fp, const char* name, const char* mode);
 #endif
 namespace camerasp{
+  std::string upper_case(std::string const& str);
   std::string lower_case(std::string const& str);
   Json::Value get_DOM(std::string const& path);
-  /// A string to send in responses.
-  struct url_parser {
-    std::string command;
-    std::map<std::string, std::string> queries;
-    url_parser(std::string const& s);
-  };
-  //////image capture timer
   class cam_still;
   typedef asio::basic_waitable_timer<std::chrono::steady_clock>   high_resolution_timer;
   void setTimer(high_resolution_timer& timer, cam_still&);
   //std::string  get_image(unsigned int k);
 
- bool is_integer(const std::string & s, int* k);
+  bool is_integer(const std::string & s, int* k);
 
+  RASPICAM_EXPOSURE get_exposure_from_string (const  std::string& str ); 
+
+  RASPICAM_AWB get_awb_from_string ( const std::string& str ); 
+  RASPICAM_FORMAT get_format_from_string ( const std::string& str ); 
 }
--- server/utils.cpp
+++ server/utils.cpp
@@ -14,14 +14,20 @@
 #include <fstream>
 #include <sstream>
 #include <camerasp/utils.hpp>
+#include <camerasp/raspicamtypes.h>
 namespace camerasp{
 
+
+  std::string upper_case(std::string const& str)    {
+    std::string retval(str);
+    std::transform(std::begin(str), std::end(str), std::begin(retval), toupper);
+    return retval;
+  }
   std::string lower_case(std::string const& str)    {
     std::string retval(str);
     std::transform(std::begin(str), std::end(str), std::begin(retval), tolower);
     return retval;
   }
-
 
   JSONCPP_STRING read_file_content(const char* path)    {
     std::ifstream ifs(path);
@@ -56,4 +62,42 @@ namespace camerasp{
 
     return true;//(*p == 0);
   }
+  RASPICAM_EXPOSURE get_exposure_from_string (const  std::string& str ) {
+    if ( str=="OFF" ) return RASPICAM_EXPOSURE_OFF;
+    if ( str=="AUTO" ) return RASPICAM_EXPOSURE_AUTO;
+    if ( str=="NIGHT" ) return RASPICAM_EXPOSURE_NIGHT;
+    if ( str=="NIGHTPREVIEW" ) return RASPICAM_EXPOSURE_NIGHTPREVIEW;
+    if ( str=="BACKLIGHT" ) return RASPICAM_EXPOSURE_BACKLIGHT;
+    if ( str=="SPOTLIGHT" ) return RASPICAM_EXPOSURE_SPOTLIGHT;
+    if ( str=="SPORTS" ) return RASPICAM_EXPOSURE_SPORTS;
+    if ( str=="SNOW" ) return RASPICAM_EXPOSURE_SNOW;
+    if ( str=="BEACH" ) return RASPICAM_EXPOSURE_BEACH;
+    if ( str=="VERYLONG" ) return RASPICAM_EXPOSURE_VERYLONG;
+    if ( str=="FIXEDFPS" ) return RASPICAM_EXPOSURE_FIXEDFPS;
+    if ( str=="ANTISHAKE" ) return RASPICAM_EXPOSURE_ANTISHAKE;
+    if ( str=="FIREWORKS" ) return RASPICAM_EXPOSURE_FIREWORKS;
+    return RASPICAM_EXPOSURE_AUTO;
+  }
+
+  RASPICAM_AWB get_awb_from_string ( const std::string& str ) {
+    if ( str=="OFF" ) return RASPICAM_AWB_OFF;
+    if ( str=="AUTO" ) return RASPICAM_AWB_AUTO;
+    if ( str=="SUNLIGHT" ) return RASPICAM_AWB_SUNLIGHT;
+    if ( str=="CLOUDY" ) return RASPICAM_AWB_CLOUDY;
+    if ( str=="SHADE" ) return RASPICAM_AWB_SHADE;
+    if ( str=="TUNGSTEN" ) return RASPICAM_AWB_TUNGSTEN;
+    if ( str=="FLUORESCENT" ) return RASPICAM_AWB_FLUORESCENT;
+    if ( str=="INCANDESCENT" ) return RASPICAM_AWB_INCANDESCENT;
+    if ( str=="FLASH" ) return RASPICAM_AWB_FLASH;
+    if ( str=="HORIZON" ) return RASPICAM_AWB_HORIZON;
+    return RASPICAM_AWB_AUTO;
+  }
+  RASPICAM_FORMAT get_format_from_string ( const std::string& str ) {
+    if(str=="GREY") return RASPICAM_FORMAT_GRAY;
+    if(str=="RGB") return RASPICAM_FORMAT_RGB;
+    if(str=="YUV") return RASPICAM_FORMAT_YUV420;
+    return RASPICAM_FORMAT_RGB;
+  }
+
 }
+
