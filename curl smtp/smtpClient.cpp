405856420e62add01a4279387d250540ce5579af
camerasp <joseph.mariadassou@outlook.com>
2017-05-31 19:34:05 +1000

    smoke tested mailer

--- curl smtp/smtpClient.cpp
+++ curl smtp/smtpClient.cpp
@@ -20,9 +20,10 @@
  /* Note that this example requires libcurl 7.20.0 or above.
  */
 
-#define FROM    "<joe.mariadassou@gmail.com>"
-#define TO      "<theSundayProgrammer@gmail.com>"
-#define CC      "<joseph.mariadassou@outlook.com>"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+
 
 
 struct upload_status {
@@ -46,19 +47,6 @@ static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp)
 
 }
 
-static const char *payload_text[] = {
-"Date: Wed, 30 May 2017 08:54:29 +1100\r\n",
-  "To: " TO "\r\n",
-  "From: " FROM "Chakra\r\n",
-  "Message-ID: <dcd7cb3r9-11db-487a-9f3a-e652a9458efd@"
-  "theSundayProgrammer:.org>\r\n",
-  "Subject: SMTP TLS message\r\n",
-  "\r\n", /* empty line to divide headers from body, see RFC5322 */
-  "The body of the message starts here.\r\n",
-  "\r\n",
-  "It could be a lot of lines, could be MIME encoded, whatever.\r\n",
-  "Check RFC5322.\r\n"
-};
 class smtp_client
 {
   public:
@@ -71,39 +59,68 @@ class smtp_client
     string user, password;
     string url;
     string from;
-    std::vector<string> message;
-    CURL *curl;
+    string body;
+    string subject;
+    std::vector<string> recipient_ids;
     CURLcode send()
     {
-      curl_easy_setopt(curl, CURLOPT_USERNAME, "joe.mariadassou@gmail.com");
-      curl_easy_setopt(curl, CURLOPT_PASSWORD, "954Vnrtz");
-      curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.gmail.com:587");
+      string date ( "Date: Wed, 30 May 2017 08:54:29 +1100\r\n");
+      message.push_back(date);
+      string to = "To: ";
+      to += *recipient_ids.begin() + "\r\n";
+      message.push_back(to);
+
+      std::cerr << "OK Here " << __LINE__ << std::endl;
+      message.push_back(string("From: ") + from + "\r\n");
+
+
+      boost::uuids::random_generator gen;
+      boost::uuids::uuid u = gen();
+      message.push_back(string("Message-ID: ") + "<" + to_string(u) + "@theSundayProgrammer.com>\r\n");
+
+      message.push_back(string("Subject :") + subject + "\r\n");
+      message.push_back(string("\r\n"));
+      message.push_back(body);
+      message.push_back(string("\r\n"));
+
+      curl_easy_setopt(curl, CURLOPT_USERNAME, user.c_str());
+      curl_easy_setopt(curl, CURLOPT_PASSWORD, password.c_str());
+      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
       curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
       curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
       curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
       curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 
-      curl_easy_setopt(curl, CURLOPT_MAIL_FROM, FROM);
+      curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from.c_str() );
 
-      curl_slist *recipients = curl_slist_append(nullptr, TO);
-      recipients = curl_slist_append(recipients, CC);
+
+      curl_slist *recipients = nullptr;
+      for(auto const& str: recipient_ids)
+{
+	recipients = curl_slist_append(recipients, str.c_str());
+      std::cerr <<str<< std::endl;
+}
       curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
 
+      std::cerr << "OK Here " << __LINE__ << std::endl;
       curl_easy_setopt(curl, CURLOPT_READFUNCTION, smtp_client::callback);
       curl_easy_setopt(curl, CURLOPT_READDATA, this);
       curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
 
       curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
+      std::cerr << "OK Here " << __LINE__ << std::endl;
       start = message.begin();
       finish = message.end();
       CURLcode res = curl_easy_perform(curl);
 
+      std::cerr << "OK Here " << __LINE__ << std::endl;
       if (res != CURLE_OK)
 	fprintf(stderr, "curl_easy_perform() failed: %s\n",
 	    curl_easy_strerror(res));
       curl_slist_free_all(recipients);
 
+      std::cerr << "OK Here " << __LINE__ << std::endl;
     }
     ~smtp_client()
     {
@@ -111,6 +128,8 @@ class smtp_client
     }
   private:
     std::vector<std::string>::iterator start,finish;
+    std::vector<string> message;
+    CURL *curl;
     static size_t callback(void *ptr, size_t size, size_t nmemb, void *userp)
     {
       smtp_client *this_= static_cast<smtp_client*>(userp);
@@ -129,14 +148,26 @@ class smtp_client
     }
 };
     
+#define TO      "<theSundayProgrammer@gmail.com>"
+#define CC      "<joseph.mariadassou@outlook.com>"
 
 int main(void)
 {
   CURLcode res = CURLE_OK;
   smtp_client smtp; 
 
-  for(auto it= std::begin(payload_text); it!=std::end(payload_text); ++it)
-    smtp.message.push_back(*it);  
+  smtp.user= "joe.mariadassou@gmail.com";
+  smtp.password =  "954Vnrtz";
+  smtp.url="smtp://smtp.gmail.com:587";
+  smtp.from = "<joe.mariadassou@gmail.com> Chakra";
+  smtp.subject = "Test Message";
+  smtp.body = "This is test message\r\n" 
+  "It could be a lot of lines, could be MIME encoded, whatever.\r\n"
+  "Check RFC5322.\r\n";
+   smtp.recipient_ids.push_back(TO);
+   smtp.recipient_ids.push_back(CC);
+      std::cerr << "OK Here " << __LINE__ << std::endl;
   smtp.send();
+
   return (int)res;
 }
